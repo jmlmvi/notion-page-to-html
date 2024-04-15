4b82f4da70e71aef9753949a5d7a290825be270c
Jean-Marc Henry <92588060+jmlmvi@users.noreply.github.com>
2024-04-15 14:07:14 +0200

    Update code.ts

--- src/data/use-cases/blocks-to-html-converter/block-parsers/code.ts
+++ src/data/use-cases/blocks-to-html-converter/block-parsers/code.ts
@@ -8,14 +8,26 @@ export class CodeBlockToHtml implements ToHtml {
   constructor(block: Block) {
     this._block = block;
   }
-
-  async convert(): Promise<string> {
+// Modification jmh 15/04/2024
+//  async convert(): Promise<string> {
+//    const languageClass = this._language ? `class="language-${this._language}"` : '';
+//
+//    return Promise.resolve(
+//      `<pre><code ${languageClass}>${blockToInnerText(this._block).replace(/(\s{4}|\t)/g, '  ')}</code></pre>`,
+//    );
+//  }
+async convert(): Promise<string> {
     const languageClass = this._language ? `class="language-${this._language}"` : '';
 
+    // Obtient le texte du bloc et remplace les tabulations par deux espaces.
+    // Aucun remplacement supplémentaire pour les retours à la ligne n'est nécessaire avec <pre>.
+    const innerText = blockToInnerText(this._block).replace(/(\t)/g, '  ');
+
     return Promise.resolve(
-      `<pre><code ${languageClass}>${blockToInnerText(this._block).replace(/(\s{4}|\t)/g, '  ')}</code></pre>`,
+      `<pre><code ${languageClass}>${innerText}</code></pre>`
     );
-  }
+}
+// Fin Modification jmh 15/04/2024
 
   private get _language(): string {
     return this._block.properties?.language?.toLowerCase().replace(/ /g, '');
