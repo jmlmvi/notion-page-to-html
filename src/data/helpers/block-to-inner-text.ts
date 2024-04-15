102502e82a9c9ac1879890cd86e76ca161a1b6d6
Jean-Marc Henry <92588060+jmlmvi@users.noreply.github.com>
2024-04-15 16:40:29 +0200

    ajout du /n

--- src/data/helpers/block-to-inner-text.ts
+++ src/data/helpers/block-to-inner-text.ts
@@ -2,5 +2,5 @@ import { Block } from '../../data/protocols/blocks';
 
 export const blockToInnerText = (block: Block): string => {
   const decorableTexts = block.decorableTexts;
-  return decorableTexts ? decorableTexts.map((dt) => dt.text).join('') : '';
+  return decorableTexts ? decorableTexts.map((dt) => dt.text).join('\n') : '';
 };
